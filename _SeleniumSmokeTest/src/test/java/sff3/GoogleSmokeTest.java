96ef68e5c3d934f779c22d5b8c917a36420c341f
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-15 03:29:35 -0700

    added interface and enum code

--- _SeleniumSmokeTest/src/test/java/sff3/GoogleSmokeTest.java
+++ _SeleniumSmokeTest/src/test/java/sff3/GoogleSmokeTest.java
@@ -1,5 +1,30 @@
 package sff3;
 
+import org.openqa.selenium.WebDriver;
+
+interface DriverConfig {
+	public WebDriver getWebDriver();
+}
+
+enum DriverType implements DriverConfig {
+	CHROME{
+		public WebDriver getWebDriver() {
+			return null;
+		}
+	},
+	FIREFOX{
+		public WebDriver getWebDriver() {
+			return null;
+		}
+	},
+	EDGE {
+		public WebDriver getWebDriver() {
+			return null;
+		}
+	}
+
+}
+
 public class GoogleSmokeTest {
 
 }
