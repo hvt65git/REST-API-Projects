06fac843f3c60702df3bb425326b52f57501ff7a
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-15 02:51:47 -0700

    first change to App main

--- _RestAssuredGoogleMaps/src/main/java/APIAutomation/SampleRestAssured/App.java
+++ _RestAssuredGoogleMaps/src/main/java/APIAutomation/SampleRestAssured/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! - first soource code change, stage, and commit to local and upstream remote repo" );
     }
 }
