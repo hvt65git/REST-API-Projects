f06f20d2f0e71612e8713c97c17a4b303a1ea27b
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-15 22:25:06 -0700

    initial commit

---
+++ _OpenWeatherMap-u2/src/test/java/APIAutomation/OpenWeatherMap/Test1.java
@@ -0,0 +1,13 @@
+package APIAutomation.OpenWeatherMap;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test()
+	private static void method1() {
+		System.out.println( "OpenWeatherMap class AppTest - method1 - Hello World!" );
+	}
+
+
+}
