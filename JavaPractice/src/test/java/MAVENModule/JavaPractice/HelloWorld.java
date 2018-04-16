0524b15d0c0e00274301af743904c280307f9ea4
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-15 22:36:26 -0700

    JavaPractice project initial commit

---
+++ JavaPractice/src/test/java/MAVENModule/JavaPractice/HelloWorld.java
@@ -0,0 +1,11 @@
+package MAVENModule.JavaPractice;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void hello() {
+		System.out.println("hello there!!");
+	}
+}
