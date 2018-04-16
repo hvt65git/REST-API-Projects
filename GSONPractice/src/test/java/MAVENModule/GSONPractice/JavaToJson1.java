b9196c1b928a741610e7b80c33447ee27aa80471
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-16 02:17:42 -0700

    initial commit of GSONPractice

---
+++ GSONPractice/src/test/java/MAVENModule/GSONPractice/JavaToJson1.java
@@ -0,0 +1,12 @@
+package MAVENModule.GSONPractice;
+
+import org.testng.annotations.Test;
+
+public class JavaToJson1 {
+	
+	@Test(enabled = true)
+	public void test01() {
+		System.out.println("*** JavaToJson1 executed ***");
+	}
+
+}
---
+++ GSONPractice/src/test/java/MAVENModule/GSONPractice/JavaToJson3.java
@@ -0,0 +1,5 @@
+package MAVENModule.GSONPractice;
+
+public class JavaToJson3 {
+
+}
