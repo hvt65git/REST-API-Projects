be45d8ad12d392c9c74a8cf71829353614d8ede8
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2018-04-30 23:06:23 -0700

    lookin good!

--- GSONPractice/src/test/java/MAVENModule/GSONPractice/JIRACreateJSON.java
+++ GSONPractice/src/test/java/MAVENModule/GSONPractice/JIRACreateJSON.java
@@ -21,6 +21,14 @@ enum JIRAType {
 		this.display = display;	
 	}
 }
+//Printing JIRAItem object:
+//Printing Project:
+//key = TEST
+//Printing Fields object:
+//Summary = REST ye merry gentlemen.
+//Description = Creating of an issue using project keys and issue type names using the REST API
+//Printing IssueType object:
+//name = Bug
 
 /*
  * Jira Task Json format:
@@ -38,24 +46,17 @@ enum JIRAType {
 }
  */
 class JIRAItem {
-	Fields fields;
+	private final Fields fields;
 
-	public JIRAItem(Fields fields) {
-		this.fields = fields;
+	public JIRAItem(Fields ff) {
+		this.fields = ff;
+	}
+
+	public void print() {
+		System.out.println("Printing JIRAItem object:");	
+		fields.print();	
 	}
 }
-
-
-//"fields": {
-//    "project": { 
-//       "key": "TEST"
-//    },
-//    "summary": "REST ye merry gentlemen.",
-//    "description": "Creating of an issue using project keys and issue type names using the REST API",
-//    "issuetype": {
-//       "name": "Bug"
-//    }
-//}
 
 class Fields {
 	Project project;
@@ -73,6 +74,14 @@ class Fields {
 		this.description = description;
 		this.issuetype = issuetype;
 	}
+
+	public void print() {
+		project.print();	
+		System.out.println("Printing Fields object:" +
+				"\r\nSummary = " + summary +
+				"\r\nDescription = " + description );	
+		issuetype.print();	
+	}
 }
 
 class Project {
@@ -81,83 +90,54 @@ class Project {
 	public Project(String key) {
 		this.key = key;
 	}
+
+	public void print() {
+		System.out.println("Printing Project:"+"\r\n" +  "key = " + this.key);
+	}
 }
 
 
 class IssueType {
 	String name;
-	public IssueType(String name) {
 
+	public IssueType(String name) {
+		this.name = name;
+	}
+
+	public void print() {
+		System.out.println("Printing IssueType object:"+"\r\n" +  "name = " + this.name);
 	}
 }
 
 public class JIRACreateJSON {
 
-	/*
-	 * Jira Task Json format:
-	 * {
-	    "fields": {
-	       "project": { 
-	          "key": "TEST"
-	       },
-	       "summary": "REST ye merry gentlemen.",
-	       "description": "Creating of an issue using project keys and issue type names using the REST API",
-	       "issuetype": {
-	          "name": "Bug"
-	       }
-	   }
-	}
-	 */
-
 	@Test(enabled = true)
 	public void test01() {
-		//create the test data as a java object
 
-		//(1) create obj:
-		//		 "issuetype": {
-		//			"name": "Bug"
-		//		 }
-		//			
-		IssueType issuetype = new IssueType("Bug");
-
-		//(2)
-		//	       "project": { 
-		//	          "key": "TEST"
-		//	       },
-		Project project = new Project("TEST");
-
-		//(3)
-		//	    "fields": {
-		//		       "project": { 
-		//		          "key": "TEST"
-		//		       },
-		//		       "summary": "REST ye merry gentlemen.",
-		//		       "description": "Creating of an issue using project keys and issue type names using the REST API",
-		//		       "issuetype": {
-		//		          "name": "Bug"
-		//		       }
-		//		   }
-		Fields fields = new Fields(project, 
-				"REST ye merry gentlemen.",
+		Fields fields = new Fields(
+				new Project("TEST"), 
+				new String("REST ye merry gentlemen."),
 				"Creating of an issue using project keys and issue type names using the REST API",
-				issuetype);
+				new IssueType("Bug"));
 
-		//(4) JIRAItem
 		JIRAItem jitem = new JIRAItem(fields);
+		System.out.println("\r\njiraItem dump before serialization => ");
+		jitem.print();
 
-		//(5)
+
 		//instantiate a GsonBuilder object and pass java obj to it
-		//use GsonBuilder to create a Gson object 
 		Gson gs = new GsonBuilder()
 				.disableHtmlEscaping()
 				.serializeNulls()
 				.create();
 
+		//convert to Json and print it out
 		String stringJson = gs.toJson(jitem);
-
 		System.out.println("stringJson = " + stringJson);
 
-
+		JIRAItem jiraItem = gs.fromJson(stringJson, JIRAItem.class);
+		System.out.println("\r\njiraItem dump after deserialization => ");
+		jiraItem.print();
 	}
 
 }
