f194a9f22efa2cdf6cde4f76d6ecaf3ba5636956
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2019-04-30 03:05:39 -0700

    final code addition on april 30,2019

--- _GeneralJavaPractice/src/test/java/test8_unitTest/StdoutToFile.java
+++ _GeneralJavaPractice/src/test/java/test8_unitTest/StdoutToFile.java
@@ -1,5 +1,12 @@
 package test8_unitTest;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -8,8 +15,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import static org.testng.Assert.assertTrue;
 
-public class testBeerMug {
-	//set test data string(s)
+public class StdoutToFile {
+	private final String filename = "C:\\Users\\nightbeats2\\eclipse-workspace\\testoutput.txt";
 	private final String testname = "Japanese Craft Beer - Pale Ale";
 
 	// Store the original standard out before changing it.
@@ -26,14 +33,34 @@ public class testBeerMug {
 	public void testMethod1() {
 		System.out.print(testname);
 
-		//ht added - this appears to be what i need to test my BeerMug class - yes, success.
+		//ht added - this appears to be what i need to test my BeerMug class-yes success!
 		assertTrue(this.consoleContent.toString().indexOf(testname)!=-1);
 	}
 
 
 	@AfterMethod
 	public void afterTest() {
-		// Put back the standard out.
+
+		//write console output to file
+		try {
+			Writer writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(filename), "utf-8")) ;
+
+			System.setOut(new PrintStream(new FileOutputStream(filename)));
+
+			System.out.println(this.consoleContent.toString());
+
+			System.out.println(
+					String.format("===>Captured Console length=%d", 
+							this.consoleContent.toString().length()));
+
+			writer.close();
+		} 
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		// Put back the standard out and output to console
 		System.setOut(this.originalStdOut);
 
 		// Print what has been captured.
@@ -45,9 +72,5 @@ public class testBeerMug {
 
 		// Clear the consoleContent.
 		this.consoleContent = new ByteArrayOutputStream();    
-		
-		
 	}
-
 }
-
