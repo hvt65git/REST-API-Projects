87ed476e4f28c7d8419fa00e31212bf44a91544e
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2019-06-15 03:01:51 -0700

    in progress

---
+++ _RestAssuredTwitter/src/main/java/ht_twit_3/GetUserTimeline.java
@@ -0,0 +1,120 @@
+package ht_twit_3;
+
+import java.util.List;
+import java.util.Map;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+import static ht_twit_3.OAUTH.accessToken;
+import static ht_twit_3.OAUTH.accessTokenSecret;
+import static ht_twit_3.OAUTH.consumerKey;
+import static ht_twit_3.OAUTH.consumerSecret;
+
+import static ht_twit_3.TwitterResource.httpOK;
+import static ht_twit_3.TwitterResource.baseURI;
+import static ht_twit_3.TwitterResource.user_timeline_endpoint;
+import static ht_twit_3.TwitterResource.countParam;
+import static ht_twit_3.TwitterResource.screenNameParam;
+
+/*
+ * 
+ */
+class OAUTH {
+	public static final String consumerKey 			= "Ez8HSiCaVZb0EW5JPctxVFz30";
+	public static final String consumerSecret 		= "1jpzfBTO4cIIMFwYT6VsSFnetdBJkK4w032Qr813vwryZj5gwS";
+	public static final String accessToken 			= "3556936994-DYhVytMQCt1quSyh20dxOKs3NNVHsbT6ebvCce7";
+	public static final String accessTokenSecret	= "pRVltgFncO2Eb0t5uUkPtywCQRwunT7MrfxJpV1dgHGkJ";
+}
+
+/*
+ * 
+ */
+class Param {
+	String name;
+	Object value;
+
+	public Param(String name, Object value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+}
+class TwitterResource {
+	public static final int httpOK = 200;
+	public static final String baseURI = "https://api.twitter.com";
+	public static final String user_timeline_endpoint = "/1.1/statuses/user_timeline.json";
+	
+	public static final Param countParam = new Param("count", 5);
+	public static final Param screenNameParam = new Param("screen_name", "realDonaldTrump");
+}
+
+/*
+ * Calls the Twitter ReST API for getting tweets, parses the response, then displays the tweets.
+ */
+public class GetUserTimeline {
+
+
+	/*
+	 * 
+	 */
+	private static Response GetTwitterUserTimeline() {
+		//issue the Get request
+		ValidatableResponse vr = 
+				RestAssured.given()
+				.auth()
+				.oauth(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+				.param(countParam.getName(), (int)countParam.getValue())
+				.param(screenNameParam.getName(), screenNameParam.getValue())
+				.contentType(ContentType.JSON)
+				.log().all()
+				.when()
+				.get(baseURI+user_timeline_endpoint)
+				.then()
+				.assertThat().statusCode(httpOK);
+
+		return vr.extract().response();
+	}
+
+	/*
+	 * 
+	 */
+	private static void printTweets(List<Map<String,Object>> list) {
+		//iterate the hash maps using an entry set
+		for(int i=0; i<list.size(); i++) {
+			for(Map.Entry<String, Object> entry : list.get(i).entrySet()) {
+				switch(entry.getKey()) {
+				case "created_at":
+					System.out.println("Tweet #" + (i+1) + " Date Created:" + entry.getValue());
+					break;
+				case "text":
+					System.out.println(entry.getValue() + "\r\n");
+					break;
+				case "retweet_count":
+				case "favorite_count":
+					System.out.println(entry.getKey() + " = "+ entry.getValue());
+					break;
+				}
+			}
+		}
+	}
+	/*
+	 * 
+	 */
+	public static void main(String[] arg) {	
+		//issue the Rest Get request for getting a Twitter user timeline
+		Response response = GetTwitterUserTimeline();
+
+		//process the JSON response and print out the tweets
+		printTweets(response.jsonPath().getList("$"));
+	}
+}
