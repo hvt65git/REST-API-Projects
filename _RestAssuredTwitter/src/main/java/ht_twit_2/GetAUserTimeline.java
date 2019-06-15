ac4fe81f15e74f55382c33f73e82a80cdd915b59
nightbeats2 <nightbeats2@LAPTOP-MTFJEHHC>
2019-06-15 00:50:12 -0700

    completed ht_twit_2 code

--- _RestAssuredTwitter/src/main/java/ht_twit_2/GetAUserTimeline.java
+++ _RestAssuredTwitter/src/main/java/ht_twit_2/GetAUserTimeline.java
@@ -1,36 +1,22 @@
-package ht_twit_1;
+package ht_twit_2;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
-import ht_twit_1.CountParam;
-import ht_twit_1.ScreenNameParam;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
 
-import static ht_twit_1.StatusCode.OK;
-import static ht_twit_1.OAUTH.accessToken;
-import static ht_twit_1.OAUTH.accessTokenSecret;
-import static ht_twit_1.OAUTH.consumerKey;
-import static ht_twit_1.OAUTH.consumerSecret;
+import static ht_twit_2.OAUTH.accessToken;
+import static ht_twit_2.OAUTH.accessTokenSecret;
+import static ht_twit_2.OAUTH.consumerKey;
+import static ht_twit_2.OAUTH.consumerSecret;
 
-import  ht_twit_1.CountParam;
-import  ht_twit_1.ScreenNameParam;
-import static ht_twit_1.Resource.get_user_timeline_resource_URL;
+import static ht_twit_2.Resource.get_user_timeline_resource_URL;
 
-/*
- * 
- */
-class StatusCode {
-	public static final int OK = 200;
-}
 
-/*
- * 
- */
 class OAUTH {
 	public static final String consumerKey 			= "Ez8HSiCaVZb0EW5JPctxVFz30";
 	public static final String consumerSecret 		= "1jpzfBTO4cIIMFwYT6VsSFnetdBJkK4w032Qr813vwryZj5gwS";
@@ -38,59 +24,22 @@ class OAUTH {
 	public static final String accessTokenSecret	= "pRVltgFncO2Eb0t5uUkPtywCQRwunT7MrfxJpV1dgHGkJ";
 }
 
-/*
- * 
- */
+
 class Resource {
 	private static final String baseURI = "https://api.twitter.com";
 	private static final String user_timeline_endpoint = "/1.1/statuses/user_timeline.json";
-	private static final String user_timeline_resource_URL = baseURI + user_timeline_endpoint;
-
+	
 	public static String get_user_timeline_resource_URL() {
-		return user_timeline_resource_URL;
+		return baseURI + user_timeline_endpoint;
 	}
 }
 
-/*
- * 
- */
-class ScreenNameParam {
-	private static final String key = "screen_name";
-	private static final String value = "realDonaldTrump";
-
-	public static String getValue() {
-		return value;
-	}
-
-	public static  String getKey() {
-		return key;
-	}
-}
-
-/*
- * 
- */
-class CountParam {
-	private static final int value = 5;
-	private static final String key = "count";
-
-	public static String getKey() {
-		return key;
-	}
-	public static int getValue() {
-		return value;
-	}
-}
-
-/*
- * 
- */
 public class GetAUserTimeline {
+	private static final int count = 20;
+	private static final int statusCodeOK = 200;
 
-	/*
-	 * 
-	 */
 	private static Response GETRequest(String resourceURL, int sCode) {
+		
 		ValidatableResponse vr = 
 				given()
 				.auth()
@@ -98,8 +47,8 @@ public class GetAUserTimeline {
 						consumerSecret, 
 						accessToken, 
 						accessTokenSecret)
-				.param(CountParam.getKey(), CountParam.getValue())
-				.param(ScreenNameParam.getKey(), ScreenNameParam.getValue())
+				.param("count", count)
+				.param("screen_name", "realDonaldTrump")
 				.contentType(ContentType.JSON)
 				.log().all()
 				.when()
@@ -111,30 +60,26 @@ public class GetAUserTimeline {
 		return vr.extract().response();
 	}
 
-	/*
-	 * 
-	 */
 	private static void printTweet(int ctr, HashMap<String, Object> map) {
-		//iterate the map using Entry objects
+		
 		for(Entry<String, Object> entry : map.entrySet()) {
-
-			//print the tweet date and the tweet text values
-			if(entry.getKey().equals("created_at")) {
+			
+			switch(entry.getKey()) {
+			case "created_at":
 				System.out.println("\r\nTweet #" + ++ctr + ": " + entry.getValue());
-			} 
-			else if(entry.getKey().equals("text") ) {
+				break;
+			case "text":
 				System.out.println(entry.getValue());
+				break;
 			}
 		}
 	}
 
-	/*
-	 * 
-	 */
+
 	public static void main(String[] arg) {	
 		try {
-			//issue the twitter user time line REST get request
-			Response response = GETRequest(get_user_timeline_resource_URL(), OK);
+			//issue the twitter user time line Get request
+			Response response = GETRequest(get_user_timeline_resource_URL(), statusCodeOK);
 
 			//store response in list of hashmaps
 			List<HashMap<String, Object>> root = response.jsonPath().getList("$");
